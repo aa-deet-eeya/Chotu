e94626d9c8e068dbf19201fddfb27a91fe1033be
aa-deet-eeya <aditya99mondal@gmail.com>
2020-09-30 11:57:28 +0530

    rebase

--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
 # Basic Discord Bot
 
 This bot acts as an simple interface to start and stop the server mainly. 
+
+
+# Getting Started 
+
+Install npm and node,
+
+run ```npm install``` to install the dependencies,
+
+run ```node app.js``` to run the bot
--- yt.js
+++ yt.js
@@ -1,89 +1,87 @@
-const discord = require('discord.js') ;
-const axios = require('axios') ;
-const querystring = require('querystring') ;
-const client = new discord.Client() ;
+const discord = require("discord.js");
+const axios = require("axios");
+const querystring = require("querystring");
+const client = new discord.Client();
 
+const token = "";
+const googleapi = "";
 
-const token = 'NzA3OTc4NzQ2MzM3OTUxNzQ0.XsTQlg.2dQMj3otbSQb7szqq2mN_maaNzk' ;
-const googleapi = 'AIzaSyDpHliP1QKd9r4vZssPid4jam4Pz5XxZ9w' ;
+client.login(token);
 
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    console.log("pong");
+    msg.reply("pong");
+  }
+});
 
-client.login(token); 
+client.on("message", (msg) => {
+  if (msg.content === "yt.") {
+    let search = {
+      q: msg.content.split("yt.")[1],
+      part: "snippet",
+      maxResult: 30,
+      key: googleapi,
+    };
 
-client.on('message', msg=>{
-    if(msg.content === 'ping'){
-        console.log('pong') ;
-        msg.reply('pong') ;
-    }
-}) ;
+    axios
+      .get(
+        "https://www.googleapis.com/youtube/v3/search?" +
+          querystring.stringify(search)
+      )
+      .then((res) => {
+        var i = 0;
 
-client.on('message' , msg => {
-    if( msg.content === 'yt.'){
-        let search = {
-            q: msg.content.split('yt.')[1] ,
-            part: "snippet" ,
-            maxResult : 30 ,
-            key : googleapi 
-        } ;
+        if (typeof res.data.items[i] === "undefined") {
+          msg.channel.send(
+            new discord.MessageEmbed()
+              .setColor("#0099ff")
+              .setTitle("404")
+              .setDescription("No video found")
+          );
+        } else {
+          const yt = (i) => {
+            if (i < res.data.items.length) {
+              let link =
+                "https://www.youtube.com/watch?v=" +
+                res.data.items[0].id.videoId;
 
-        axios.get('https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(search))
-                .then((res)=>{
-                    var i = 0;
+              var ytembed = new discord.MessageEmbed()
+                .setColor("#0099ff")
+                .setTitle(res.data.items[i].snippet.title)
+                .setURL(link)
+                .setAuthor(res.data.items[i].snippet.channelTitle)
+                .setDescription(res.data.items[i].snippet.description)
+                .setImage(res.data.items[i].snippet.thumbnails.high.url);
+            } else {
+              var ytembed = new discord.MessageEmbed()
+                .setColor("#0099ff")
+                .setTitle("No more Results")
+                .setDescription("End of search results");
+            }
 
-					if (typeof res.data.items[i] === 'undefined') {
-						msg.channel.send(
-							new discord.MessageEmbed().setColor('#0099ff').setTitle('404').setDescription('No video found')
+            msg.channel.send(ytembed);
+          };
 
-						);
-					}
+          yt(i);
 
-					else {
-
-						const yt = (i)=> {
-							if(i < (res.data.items.length) ) {
-								let link = ('https://www.youtube.com/watch?v=' + res.data.items[0].id.videoId);
-
-
-								var ytembed = new discord.MessageEmbed()
-									.setColor('#0099ff')
-									.setTitle(res.data.items[i].snippet.title)
-									.setURL(link)
-									.setAuthor(res.data.items[i].snippet.channelTitle)
-									.setDescription(res.data.items[i].snippet.description)
-									.setImage(res.data.items[i].snippet.thumbnails.high.url);
-
-							}
-						
-							else {
-								var ytembed = new discord.MessageEmbed()
-									.setColor('#0099ff')
-									.setTitle('No more Results')
-									.setDescription('End of search results');
-							}
-
-						msg.channel.send(ytembed);
-                        }
-                        
-                        yt(i) ;
-
-						yt(i).then(()=>{
-                            message.channel.awaitMessages(response => response.content === 'n' ,{
-                                max : 1,
-                                time : 15*1000,
-                                errors: ['time'] ,
-                            })
-                            .then((collected)=>{
-                                msg.channel.send('yes') ;
-                                console.log('yes') ;
-                            })
-                            .catch((collected)=>{
-                                msg.channel.send('no') ;
-                                console.log('nope') ;
-                            }) ;
-                        });
-                    }
-                }) ;
-
-    } ;
-
-}) ;
+          yt(i).then(() => {
+            message.channel
+              .awaitMessages((response) => response.content === "n", {
+                max: 1,
+                time: 15 * 1000,
+                errors: ["time"],
+              })
+              .then((collected) => {
+                msg.channel.send("yes");
+                console.log("yes");
+              })
+              .catch((collected) => {
+                msg.channel.send("no");
+                console.log("nope");
+              });
+          });
+        }
+      });
+  }
+});
